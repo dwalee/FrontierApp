bf320d479de8baa91cbebf034daeea44b116af3b
Dwaine <dlee1993.dl@gmail.com>
2018-07-30 04:26:54 -0500

    Chat activity added

---
+++ app/src/main/java/com/frontierapp/frontierapp/chat.java
@@ -0,0 +1,14 @@
+package com.frontierapp.frontierapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class chat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
